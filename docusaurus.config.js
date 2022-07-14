e86f3fb97c92432f74238f99553771a5f986522e
Rixzty <landertxu2015@gmail.com>
2022-07-14 15:56:55 +0200

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -86,7 +86,7 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
+            'https://github.com/Rixzty/kirikinowiki.github.io/',
         },
         blog: {
           showReadingTime: true,
