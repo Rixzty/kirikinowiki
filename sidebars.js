a5b045ba1c86e491823e87eb21992d6684123de0
Rixzty <landertxu2015@gmail.com>
2022-07-28 01:32:52 +0200

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -2,11 +2,13 @@ module.exports = {
   someSidebar: {
     Sarrera: ['hasiera', 'erabilera'],
 	Filtroak: ['filtro-yt', 'filtro-internet', 'filtro-librea'],
-	  	Informazioa: ['galderak'],
+	  	Aplikazioa: ['aplikazioa'],
+	  		Informazioa: ['galderak'],
   },
   EspañolSidebar: {
     Introducción: ['comienzo', 'funcionamiento'],
 	Filtros: ['yt-filtro', 'internet-filtro', 'filtro-libre'],
-	  	Información: ['faq'],
+	  	Aplicación: ['aplicacion'],
+	  		Información: ['faq'],
   },
 };
